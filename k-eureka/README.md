050e46c6be6ff47e506ec679a26886e118fbc760
bikang1 <bikang1@staff.weibo.com>
2021-06-28 23:38:25 +0800

    增加安全验证

---
+++ k-eureka/README.md
@@ -0,0 +1,11 @@
+# eureka
+
+基于AP的服务注册中心
+
+初步使用
+```javascript
+1. 依赖spring-boot
+2. 引入serverce
+3. 注册服务
+
+```
--- k-eureka/eureka-server/src/main/java/cn/beckbi/EurekaServerApp.java
+++ k-eureka/eureka-server/src/main/java/cn/beckbi/EurekaServerApp.java
@@ -3,6 +3,10 @@ package cn.beckbi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  *
@@ -18,6 +22,18 @@ public class EurekaServerApp {
 
     public static void main(String[] args){
         SpringApplication.run(EurekaServerApp.class, args);
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            //关闭csrf
+            http.csrf().disable();
+            // 支持httpBasic
+            http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        }
     }
 
 }
