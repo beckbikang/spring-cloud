418eb2ac14c3e51b6a79672bf1c0c22319b517dd
bikang1 <bikang1@staff.weibo.com>
2021-07-11 20:22:15 +0800

    add lib

---
+++ k-ribbon/s1/src/main/java/cn/beckbi/EurekaStateChangeListener.java
@@ -0,0 +1,43 @@
+package cn.beckbi;
+
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.cloud.netflix.eureka.server.event.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: spring-cloud
+ * @description:
+ * @author: bikang
+ * @create: 2021-06-28 23:30
+ */
+@Component
+public class EurekaStateChangeListener {
+
+    @EventListener
+    public void listen(EurekaInstanceCanceledEvent event) {
+        System.err.println(event.getServerId() + "\t" + event.getAppName() + " 服务下线");
+    }
+
+    @EventListener
+    public void listen(EurekaInstanceRegisteredEvent event) {
+        InstanceInfo instanceInfo = event.getInstanceInfo();
+        System.err.println(instanceInfo.getAppName() + "进行注册");
+    }
+
+    @EventListener
+    public void listen(EurekaInstanceRenewedEvent event) {
+        System.err.println(event.getServerId() + "\t" + event.getAppName() + " 服务进行续约");
+    }
+
+    @EventListener
+    public void listen(EurekaRegistryAvailableEvent event) {
+        System.err.println("注册中心 启动");
+    }
+
+    @EventListener
+    public void listen(EurekaServerStartedEvent event) {
+        System.err.println("Eureka Server 启动");
+    }
+
+}
