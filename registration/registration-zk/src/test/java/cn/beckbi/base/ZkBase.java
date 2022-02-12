9b10de066b317caa9530ffc06892b5b77e88a308
bikang1 <bikang1@staff.weibo.com>
2022-02-12 19:08:25 +0800

    add zk operator

--- registration/registration-zk/src/test/java/cn/beckbi/base/ZkBase.java
+++ registration/registration-zk/src/test/java/cn/beckbi/base/ZkBase.java
@@ -3,12 +3,18 @@ package cn.beckbi.base;
 import cn.beckbi.TestApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.*;
+import org.apache.curator.framework.recipes.locks.InterProcessLock;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.zookeeper.AsyncCallback;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Objects;
 
@@ -108,8 +114,267 @@ public class ZkBase extends TestApplication {
         }
 
 
-
-
-        curatorFramework.close();
     }
+
+    @Test
+    public void testLock() throws Exception{
+
+        curatorFramework.start();
+
+
+        String parentPath = "/zk/test";
+        List<String> children = curatorFramework.getChildren().forPath(parentPath);
+
+        for (String child : children) {
+            log.info("child-{}", child);
+        }
+
+
+        //test lock
+        final InterProcessMutex mutex = new InterProcessMutex(curatorFramework, "/zk/test/mutex");
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        int count = 0;
+                        mutex.acquire();
+                        for (int j = 0; j < 10; j++) {
+                            count++;
+                        }
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        log.info("count = " + count);
+                        mutex.release();
+
+                    } catch (Exception e) {
+                        log.info("error", e);
+                    }
+
+                }
+            }).start();
+
+        }
+        Thread.sleep(2000);
+    }
+
+
+    @Test
+    public void testWatch() throws Exception{
+        curatorFramework.start();
+        String workerPath = "/zk/watch/data2";
+
+        Stat stat = curatorFramework.checkExists().forPath(workerPath);
+        if (Objects.isNull(stat)) {
+            curatorFramework.create()
+                       .creatingParentsIfNeeded()
+                    .withMode(CreateMode.PERSISTENT)
+                    .forPath(workerPath, null);
+        }
+
+        Watcher watcher = new Watcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                log.info("data change:{}", watchedEvent);
+            }
+        };
+        curatorFramework.getData().usingWatcher(watcher).forPath(workerPath);
+
+        curatorFramework.setData().forPath(workerPath, "first-data".getBytes());
+
+        curatorFramework.setData().forPath(workerPath, "second-data".getBytes());
+
+
+    }
+
+    @Test
+    public void testNodeWatch() throws Exception{
+
+        curatorFramework.start();
+        String workerPath = "/zk/watch/data2";
+
+        Stat stat = curatorFramework.checkExists().forPath(workerPath);
+        if (Objects.isNull(stat)) {
+            curatorFramework.create()
+                    .creatingParentsIfNeeded()
+                    .withMode(CreateMode.PERSISTENT)
+                    .forPath(workerPath, null);
+        }
+
+        NodeCache nodeCache = new NodeCache(curatorFramework, workerPath, false);
+        NodeCacheListener nodeCacheListener = new NodeCacheListener() {
+            @Override
+            public void nodeChanged() throws Exception {
+                ChildData childData = nodeCache.getCurrentData();
+                log.info("change:path:{}", childData.getPath());
+                log.info("change:data:{}", childData.getData().toString());
+                log.info("change:stat:{}", childData.getStat());
+            }
+        };
+        nodeCache.getListenable().addListener(nodeCacheListener);
+        nodeCache.start();
+
+        curatorFramework.setData().forPath(workerPath, "first-data".getBytes());
+
+        Thread.sleep(1000);
+
+        curatorFramework.setData().forPath(workerPath, "second-data".getBytes());
+
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void testNodeWatchChild() throws Exception{
+
+        curatorFramework.start();
+        String workerPath = "/zk/watch/data2";
+
+        Stat stat = curatorFramework.checkExists().forPath(workerPath);
+        if (Objects.isNull(stat)) {
+            curatorFramework.create()
+                    .creatingParentsIfNeeded()
+                    .withMode(CreateMode.PERSISTENT)
+                    .forPath(workerPath, null);
+        }
+
+        PathChildrenCache pathChildrenCache = new PathChildrenCache(curatorFramework, workerPath, true);
+        PathChildrenCacheListener pathChildrenCacheListener =
+                new PathChildrenCacheListener() {
+                    @Override
+                    public void childEvent(CuratorFramework client,
+                                           PathChildrenCacheEvent event) {
+                        try {
+                            ChildData data = event.getData();
+                            switch (event.getType()) {
+                                case CHILD_ADDED:
+                                    log.info("子节点增加, path={}, data={}",
+                                            data.getPath(), new String(data.getData(), "UTF-8"));
+
+                                    break;
+                                case CHILD_UPDATED:
+                                    log.info("子节点更新, path={}, data={}",
+                                            data.getPath(), new String(data.getData(), "UTF-8"));
+                                    break;
+                                case CHILD_REMOVED:
+                                    log.info("子节点删除, path={}, data={}",
+                                            data.getPath(), new String(data.getData(), "UTF-8"));
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+        pathChildrenCache.getListenable().addListener(pathChildrenCacheListener);
+        pathChildrenCache.start(PathChildrenCache.StartMode.BUILD_INITIAL_CACHE);
+        Thread.sleep(1000);
+
+        for (int i=0; i < 3; ++i) {
+            String workerPathChild = workerPath+"/node-"+i;
+
+            String nodeStr = curatorFramework.create()
+                    .creatingParentsIfNeeded()
+                    .withMode(CreateMode.PERSISTENT_SEQUENTIAL)
+                    .forPath(workerPathChild, ("abc"+i).getBytes());
+
+            Stat stat2 = curatorFramework.checkExists().forPath(workerPath);
+
+        }
+        Thread.sleep(1000);
+        curatorFramework.delete().deletingChildrenIfNeeded().forPath(workerPath);
+    }
+
+
+    @Test
+    public void testTreeNodeWatch() throws Exception{
+
+        curatorFramework.start();
+        String workerPath = "/zk/watch/data2";
+
+        Stat stat = curatorFramework.checkExists().forPath(workerPath);
+        if (Objects.isNull(stat)) {
+            curatorFramework.create()
+                    .creatingParentsIfNeeded()
+                    .withMode(CreateMode.PERSISTENT)
+                    .forPath(workerPath, "test".getBytes());
+        }
+
+
+            TreeCache treeCache =
+                    new TreeCache(curatorFramework, workerPath);
+            TreeCacheListener l =
+                    new TreeCacheListener() {
+                        @Override
+                        public void childEvent(CuratorFramework client,
+                                               TreeCacheEvent event) {
+                            try {
+                                ChildData data = event.getData();
+                                if (data == null) {
+                                    log.info("data is null");
+                                    return;
+                                }
+                                switch (event.getType()) {
+                                    case NODE_ADDED:
+
+                                        log.info("[TreeCache]节点增加, path={}, data={}",
+                                                data.getPath(), new String(data.getData(), "UTF-8"));
+
+                                        break;
+                                    case NODE_UPDATED:
+                                        log.info("[TreeCache]节点更新, path={}, data={}",
+                                                data.getPath(), new String(data.getData(), "UTF-8"));
+                                        break;
+                                    case NODE_REMOVED:
+                                        log.info("[TreeCache]节点删除, path={}, data={}",
+                                                data.getPath(), new String(data.getData(), "UTF-8"));
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                            } catch (
+                                    UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+            treeCache.getListenable().addListener(l);
+            treeCache.start();
+
+
+        curatorFramework.setData().forPath(workerPath, "first-data".getBytes());
+
+        Thread.sleep(1000);
+
+        curatorFramework.setData().forPath(workerPath, "second-data".getBytes());
+
+        Thread.sleep(1000);
+
+        Thread.sleep(1000);
+
+        for (int i=0; i < 3; ++i) {
+            String workerPathChild = workerPath+"/node-"+i;
+
+            String nodeStr = curatorFramework.create()
+                    .creatingParentsIfNeeded()
+                    .withMode(CreateMode.PERSISTENT_SEQUENTIAL)
+                    .forPath(workerPathChild, ("abc"+i).getBytes());
+
+            Stat stat2 = curatorFramework.checkExists().forPath(workerPath);
+
+        }
+        Thread.sleep(1000);
+        curatorFramework.delete().deletingChildrenIfNeeded().forPath(workerPath);
+    }
+
+
+
 }
+
