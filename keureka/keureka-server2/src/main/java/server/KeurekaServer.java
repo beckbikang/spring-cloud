4911b897b622e9fc6e233f4a8ebe25f991e27958
bikang1 <bikang1@staff.weibo.com>
2022-07-24 18:20:46 +0800

    add maven

--- keureka/keureka-server2/src/main/java/server/KeurekaServer.java
+++ keureka/keureka-server2/src/main/java/server/KeurekaServer.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 /**
  * @program: spring-cloud
  * @description: eureka server
  * @author: bikang
  * @create: 2022-02-07 22:29
  */
+//@EnableZipkinServer
 @EnableEurekaServer
 @SpringBootApplication
 public class KeurekaServer {
