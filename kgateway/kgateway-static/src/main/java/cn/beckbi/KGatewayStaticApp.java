1a64845c34971e6e9fa0b4bfe32072d2458d1bba
bikang1 <bikang1@staff.weibo.com>
2022-08-15 23:16:35 +0800

    add code

---
+++ kgateway/kgateway-static/src/main/java/cn/beckbi/KGatewayStaticApp.java
@@ -0,0 +1,11 @@
+package cn.beckbi;
+
+/**
+ * @program: spring-cloud
+ * @description: KGatewayStaticApp
+ * @author: bikang
+ * @create: 2022-08-15 23:10
+ */
+
+public class KGatewayStaticApp {
+}
