db90d34f3d814a7d451556b4a378f1bc7858818e
bikang1 <bikang1@staff.weibo.com>
2022-08-16 23:04:28 +0800

    add gateway code

--- kgateway/kgateway-static/src/main/java/cn/beckbi/KGatewayStaticApp.java
+++ kgateway/kgateway-static/src/main/java/cn/beckbi/KGatewayStaticApp.java
@@ -1,11 +1,17 @@
 package cn.beckbi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @program: spring-cloud
  * @description: KGatewayStaticApp
  * @author: bikang
  * @create: 2022-08-15 23:10
  */
-
+@SpringBootApplication
 public class KGatewayStaticApp {
+    public static void main(String[] args) {
+        SpringApplication.run(KGatewayStaticApp.class, args);
+    }
 }
