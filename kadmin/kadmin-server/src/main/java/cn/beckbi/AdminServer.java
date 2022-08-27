7f5d2261589c19b7ec5b43da64aa447eaadeefd9
bikang1 <bikang1@staff.weibo.com>
2022-08-27 18:28:31 +0800

    add spring-boot-admin

---
+++ kadmin/kadmin-server/src/main/java/cn/beckbi/AdminServer.java
@@ -0,0 +1,46 @@
+package cn.beckbi;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
+ * @program: spring-cloud
+ * @description:
+ * @author: bikang
+ * @create: 2022-08-27 11:34
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServer.class);
+    }
+
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        private final String adminContextPath;
+
+        public SecurityPermitAllConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and().httpBasic()
+                    .and().csrf().disable();
+        }
+    }
+}
