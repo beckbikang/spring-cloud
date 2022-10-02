36cceeb8d71cdab917ef9b0f7936afefc82f004c
bikang1 <bikang1@staff.weibo.com>
2022-10-02 16:22:18 +0800

    add swagger

--- kstart/kstart-main/src/main/java/cn/beckbi/controller/UserController.java
+++ kstart/kstart-main/src/main/java/cn/beckbi/controller/UserController.java
@@ -2,14 +2,26 @@ package cn.beckbi.controller;
 
 
 import cn.beckbi.build.id.IdMaker;
+import cn.beckbi.response.IdData;
+import cn.beckbi.response.Result;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.Api;
 import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * @program: spring-cloud
@@ -17,6 +29,7 @@ import javax.annotation.Resource;
  * @author: bikang
  * @create: 2022-02-09 23:04
  */
+@Api(tags={"用户接口"})
 @RestController
 public class UserController {
 
@@ -26,18 +39,25 @@ public class UserController {
     @Resource
     private IdMaker idMaker;
 
-    @GetMapping("/user/{uid}")
-    public String info(@PathVariable long uid) throws JsonProcessingException {
-
-        @Data
-        class IdData {
-            private String id;
-            private Long uid;
-        }
-
+    @ApiOperation(value = "查询用户")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="uid",value="用户ID",dataType="long", paramType = "query", required=true, defaultValue="1")
+    })
+    @ApiResponses({ @ApiResponse(code = 200, message = "OK", response = Result.class) })
+    @GetMapping("/user")
+    public Result<IdData> info2(@RequestParam(name = "uid") long uid) {
         IdData idData = new IdData();
         idData.setUid(uid);
         idData.setId(idMaker.getId());
-        return mapper.writeValueAsString(idData);
+        return Result.<IdData>builder().code(0).result(idData).build();
+    }
+
+
+    @GetMapping("/user/{uid}")
+    public IdData info(@PathVariable long uid) {
+        IdData idData = new IdData();
+        idData.setUid(uid);
+        idData.setId(idMaker.getId());
+        return idData;
     }
 }
